696009d9bef6b66f01a3313ce17342f2a93f2b6d
Prashil S Gupta <33949630+Prashil1999@users.noreply.github.com>
2020-10-25 19:10:55 +0530

    reset and start

--- src/components/App.js
+++ src/components/App.js
@@ -5,12 +5,32 @@ const App = () => {
   const [renderBall, setRenderBall] = useState(false);
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
-  const [ballPosition,setBallPosition] = useState({
+  const [ballPosition, setBallPosition] = useState({
     left: "0px",
-    top: "0px",
+    top: "0px"
   });
-  const reset = () => {};
-  const renderChoice = () => {};
+  const reset = () => {
+    setRenderBall(false);
+    setX(0);
+    setY(0);
+    let ballPositionCopy = { ...ballPosition };
+    ballPositionCopy.left = x + "px";
+    ballPositionCopy.top = y + "px";
+    setBallPosition(ballPositionCopy);
+  };
+  const buttonClickHandler = () => {
+    setRenderBall(true);
+  };
+  const renderChoice = () => {
+    if (renderBall) {
+      return <div className="ball" style={ballPosition}></div>;
+    } else
+      return (
+        <button className="start" onClick={buttonClickHandler}>
+          Click For One Ball
+        </button>
+      );
+  };
 
   return (
     <div className="playground">
